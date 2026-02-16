1d8542b8fd6cad42ec3d1700091fdc261e35be3c
Eddy Ma <ocean_ltd@hotmail.com>
2026-02-15 20:48:34 -0800

    fix

--- src/models/transformer_diffusion/__init__.py
+++ src/models/transformer_diffusion/__init__.py
@@ -4,6 +4,7 @@ from .transformer_diffusion_policy import TransformerDiffusionPolicy
 from .transformer_diffusion_model import DiffusionTransformer
 from .transformer_diffusion_config import TransformerDiffusionConfig
 from .grid_overlay_processor import GridOverlayProcessorStep, draw_grid_overlay
+from .image_resize_processor import ImageResizeProcessorStep
 from .processor_transformer_diffusion import make_pre_post_processors
 
-__all__ = ["TransformerDiffusionPolicy", "DiffusionTransformer", "TransformerDiffusionConfig", "make_pre_post_processors", "GridOverlayProcessorStep", "draw_grid_overlay"]
+__all__ = ["TransformerDiffusionPolicy", "DiffusionTransformer", "TransformerDiffusionConfig", "make_pre_post_processors", "GridOverlayProcessorStep", "ImageResizeProcessorStep", "draw_grid_overlay"]
